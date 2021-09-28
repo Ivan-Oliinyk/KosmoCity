efe983c6ec0698215750f9ecd6859bc16cc1be83
Ivan <lorf1991@gmail.com>
2021-09-28 17:56:24 +0300

    start project

---
+++ index.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector(".js-burger");
+const dropContainer = document.querySelector(".js-drop-container");
+
+burger.addEventListener("click", addIsActive);
+
+function addIsActive() {
+  burger.classList.toggle("is-active");
+  dropContainer.classList.toggle("is-active");
+}
