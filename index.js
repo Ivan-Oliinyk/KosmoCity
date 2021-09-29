b68eda8d3effa06053f8b08a48bfa3aac7bb6f1a
Ivan <lorf1991@gmail.com>
2021-09-29 22:09:18 +0300

    complite section hero

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const burger = document.querySelector(".js-burger");
-const dropContainer = document.querySelector(".js-drop-container");
+window.addEventListener("DOMContentLoaded", () => {
+  //Меню и кнопка меню
+  const burger = document.querySelector(".js-burger");
+  const dropContainer = document.querySelector(".js-drop-container");
 
-burger.addEventListener("click", addIsActive);
+  burger.addEventListener("click", addIsActive);
 
-function addIsActive() {
-  burger.classList.toggle("is-active");
-  dropContainer.classList.toggle("is-active");
-}
+  function addIsActive() {
+    burger.classList.toggle("is-active");
+    dropContainer.classList.toggle("is-active");
+  }
+
+  //Появление блока в хедере
+  window.addEventListener("load", () => {
+    const hideBlocks = document.querySelectorAll(".js-hide-block");
+
+    setTimeout(() => {
+      hideBlocks.forEach((elem) => elem.classList.remove("hide-block"));
+    }, 2000);
+  });
+
+  //Счетчик в блоке промо
+  const counterLists = document.querySelectorAll(".js-counter");
+
+  addCountOnListsElement(counterLists);
+
+  function addCountOnListsElement(counterLists) {
+    for (let i = 0; i < counterLists.length; i += 1) {
+      counterLists[i].textContent = i + 1;
+    }
+  }
+});
