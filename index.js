4a9ab9c909c685f9108158cdd9dc46ad72fbfa0a
Ivan <lorf1991@gmail.com>
2021-10-02 12:05:37 +0300

    fix section catalog

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const backdropRefs = {
     wrapper: document.querySelector(".js-catalog-backdrop"),
     theme: document.querySelectorAll(".js-catalog-backdrop__theme"),
+    btnClose: document.querySelector(".js-close"),
   };
 
   catalogBtn.forEach((item, index) =>
@@ -42,10 +43,22 @@ window.addEventListener("DOMContentLoaded", () => {
       backdropRefs.wrapper.classList.remove("display-none");
       hideThemeBlock();
       backdropRefs.theme[index].classList.remove("display-none");
+      backdropRefs.btnClose.classList.remove("display-none");
     })
   );
 
+  backdropRefs.btnClose.addEventListener("click", () => {
+    hideThemeBlock();
+    hideBtnClose();
+
+    backdropRefs.wrapper.classList.add("display-none");
+  });
+
   function hideThemeBlock() {
     backdropRefs.theme.forEach((block) => block.classList.add("display-none"));
+  }
+
+  function hideBtnClose() {
+    backdropRefs.btnClose.classList.add("display-none");
   }
 });
